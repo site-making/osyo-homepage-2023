f608fbb0670193e5c99064ee567905f8c23e0cfb
basaa0283 <basaa0283@gmail.com>
2023-04-11 22:55:57 +0900

    fix modal design

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const modal = $("#js-modal");
 const overlay = $("#js-overlay");
 const close = $("#js-close");
 const open = $("#js-open");
+const body =$('body');
 
 open.on('click', function () { //ボタンをクリックしたら
   modal.addClass("open"); // modalクラスにopenクラス付与
+  body.css('overflow-y','hidden');
   overlay.addClass("open"); // overlayクラスにopenクラス付与
 });
 close.on('click', function () { //×ボタンをクリックしたら
